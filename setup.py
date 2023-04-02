53a26c63a95c00b8f18159cf4cf59b26a0834235
Penghui Yang <59365168+Paul-YPH@users.noreply.github.com>
2023-04-02 21:01:17 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import os
+
+setup(
+    name='spatrio',
+    version='1.0.0',
+    description='Spatial mapping of multi-omics data at single-cell resolution.',
+    author='',
+    author_email='',
+    url='https://github.com/ZJUFanLab/SpaTrio',
+    packages=['spatrio'],
+    install_requires=[],
+    license='GNU General Public License v3.0',
+)
