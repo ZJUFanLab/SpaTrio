07290093479fb43e65db519d28e5e93659ae7a6d
Penghui Yang <59365168+Paul-YPH@users.noreply.github.com>
2023-09-06 20:36:40 +0800

    Add files via upload

---
+++ .history/run_20230906112638.py
@@ -0,0 +1,93 @@
+import spatrio
+import pandas as pd
+import argparse
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+parser = argparse.ArgumentParser(description='Spatrio argparse')
+
+# load_data
+parser.add_argument('--input_path', default='', help='Path to read in input data')
+parser.add_argument('--ref_path', default=None, help='Path to read in reference data')
+# process_input
+parser.add_argument('--marker_use', default=True, type=str2bool)
+parser.add_argument('--top_marker_num', default=100, type=int)
+parser.add_argument('--hvg_use', default=False, type=str2bool)
+# ot_alignment
+parser.add_argument('--alpha', default=0.1, type=float)
+parser.add_argument('--dissimilarity', default='scaled_euc', type=str)
+parser.add_argument('--k', default=10, type=int)
+parser.add_argument('--graph_mode', default='connectivity', type=str)
+parser.add_argument('--aware_spatial', default=True, type=str2bool)
+parser.add_argument('--aware_multi', default=True, type=str2bool)
+parser.add_argument('--aware_power', default=2, type=int)
+# assign_coord
+parser.add_argument('--top_num', default=5, type=int)
+parser.add_argument('--random', default=False, type=str2bool)
+# save output
+parser.add_argument('--output_path', default='', help='Path to save output data')
+
+args = parser.parse_args()
+
+
+path = args.input_path
+ref_path = args.ref_path
+
+
+print('\n','***Spatrio is running***','\n')
+
+print('\n','***STEP1 LOAD DATA***','\n')
+print('Loading data...')
+spot_ann = spatrio.load_data(path+'/spatial_rna.csv' )
+single_ann = spatrio.load_data( path+'/multi_rna.csv' )
+spot_meta =  pd.read_csv(path+'/spatial_meta.csv', index_col=0)
+spot_meta['type'] = spot_meta['type'].apply(lambda x: str(x))
+single_meta =  pd.read_csv(path+'/multi_meta.csv', index_col=0)
+single_meta['type'] = single_meta['type'].apply(lambda x: str(x))
+spot_meta[['sample']] = 'spot'
+single_meta[['sample']] = 'single'
+spot_ann.obs['type'] = spot_meta['type']
+spot_ann.obs['type'] = spot_ann.obs['type'].astype(object)
+single_ann.obs['type'] = single_meta['type']
+single_ann.obs['type'] = single_ann.obs['type'].astype(object)
+print('Done!')
+
+pos = pd.read_csv(path+'/pos.csv', index_col=0)
+emb = pd.read_csv(path+'/emb.csv', index_col=0)
+spot_ann.obsm['spatial'] = pos
+single_ann.obsm['reduction'] = emb
+
+print('\n','***STEP2 PROCESS DATA***','\n')
+print('Processing data...')
+data1,data2 = spatrio.process_input(spot_ann,single_ann,
+                                    marker_use = args.marker_use,
+                                    top_marker_num = args.top_marker_num,
+                                    hvg_use = args.hvg_use)
+print('Done!')
+
+print('\n','***STEP3 OT ALIGNMENT***','\n')
+spatrio_decon = spatrio.ot_alignment(adata1 = data1, adata2 = data2, 
+                                     dissimilarity = args.dissimilarity,
+                                     alpha = args.alpha, 
+                                     k = args.k,
+                                     graph_mode = args.graph_mode,
+                                     aware_spatial = args.aware_spatial,
+                                     aware_multi = args.aware_multi,
+                                     aware_power = args.aware_power)
+print('\n','***STEP4 ASSIGN COORD***','\n')
+spatrio_map = spatrio.assign_coord(adata1 = data1,adata2 = data2,
+                                   out_data = spatrio_decon,
+                                   top_num = args.top_num,
+                                   random = args.random)
+
+print('\n','The output was saved as '+str(args.output_path)+'/output.csv')
+spatrio_map.to_csv(args.output_path+'/'+'output.csv')
---
+++ .history/spatrio_r/R/spatrio_20230906200309.R
@@ -0,0 +1,88 @@
+#' Spatrio analysis using optimal transport
+#'
+#' @param input_path Path to load input data
+#' @param output_path Path to save output data
+#' @param py_path Path to python interpreter
+#' @param spatrio_path Path to spatrio python package
+#' @param marker_use Determines whether to select differential genes of each cell/spot type for subsequent analysis
+#' @param top_marker_num Number of differential genes in each cell/spot type
+#' @param hvg_use Determines whether to only reserve highly variable genes
+#' @param alpha Alignment tuning parameter. Note: 0 <= alpha <= 1
+#' @param dissimilarity Expression dissimilarity measure
+#' @param k Number of neighbors to be used when constructing kNN graph
+#' @param graph_mode Determines whether to use a connectivity graph or a distance graph
+#' @param aware_spatial Determines whether to adjust the distance between spots according to areas (or other meta info)
+#' @param aware_multi Determines whether to adjust the distance between cells according to types (or other meta info)
+#' @param aware_power Type aware parameter. The greater the parameter, the greater the distance between different areas/types of spots/cells in the graph
+#' @param The maximum number of cells allocated in a spot
+#' @param random Determines whether to randomly assign cell coordinates or assign coordinates based on pearson correlation coefficient
+#' @param verbose
+#'
+#' @export
+#'
+
+spatrio <- function(
+    input_path = getwd(),
+    ref_path = getwd(),
+    output_path = getwd(),
+    py_path = NULL,
+    spatrio_path = NULL,
+    marker_use = TRUE,
+    top_marker_num = 100,
+    hvg_use = FALSE,
+    alpha = 0.1,
+    dissimilarity = "scaled_euc",
+    k = 10,
+    graph_mode = "connectivity",
+    aware_spatial = TRUE,
+    aware_multi = TRUE,
+    aware_power = 2,
+    top_num = 5,
+    random = FALSE,
+    verbose = TRUE,
+    ...
+) {
+  if (!dir.exists(paths = input_path)) {
+    stop("Requested input directory does not exist")
+  }
+  if (!dir.exists(paths = output_path)) {
+    stop("Requested output directory does not exist")
+  }
+  if (!dir.exists(paths = spatrio_path)) {
+    stop("Requested SpaTrio directory does not exist")
+  }
+  # find python
+  if (!file.exists("/home/yph/anaconda3/envs/spatrio/bin/python")) {
+    stop("Please enter the correct python path")
+  }else{
+    print(paste("Using the Python interpreter with a path of ",py_path,sep = ""))
+  }
+
+  cmd <- paste0(
+    py_path," ",
+    spatrio_path,'/run.py',
+    ' --input_path ',input_path,
+    ' --output_path ',output_path,
+    ' --marker_use ',marker_use,
+    ' --top_marker_num ',top_marker_num,
+    ' --hvg_use ',hvg_use,
+    ' --alpha ',alpha,
+    ' --dissimilarity ',dissimilarity,
+    ' --k ',k,
+    ' --graph_mode ',graph_mode,
+    ' --aware_spatial ',aware_spatial,
+    ' --aware_multi ',aware_multi,
+    ' --aware_power ',aware_power,
+    ' --top_num ',top_num,
+    ' --random ',random
+  )
+  
+
+  # call SpaTrio
+  system(
+    command = cmd,
+    wait = TRUE,
+    ignore.stderr = !verbose,
+    ignore.stdout = !verbose
+  )
+}
